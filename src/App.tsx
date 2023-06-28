7be61bdbaa927d8edd6824ca4385d367f6ca4f18
KKZ <KahKitZheng@gmail.com>
2023-06-28 15:08:30 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold uppercase">Lagom</h1>
+      <p className="mt-1 text-2xl text-neutral-500">
+        "Just the right amount, not too much, not too little"
+      </p>
+    </div>
+  );
+}
+
+export default App;
